507967bb5a28b4cfb126945837cc180a33ef644e
lissygonz <99147859+lissygonz@users.noreply.github.com>
2022-05-08 18:05:39 -0400

    added generateMarkdown js

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path=require("path");
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {   type:"input",
+        message:"What is your GitHub username?",
+        name:"GitHib Username:"
+    }
+    
+   
+    
+    
+];
